1d1fb1b210cd2b97bdd3f5af8b019d62e059bc04
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-10 14:22:40 +0100

    Add console with cmd module-

--- console.py
+++ console.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
 import cmd
 
-"""console module"""
+"""
+console module
+"""
 
 
 class HBNBCommand(cmd.Cmd):
-    """AirBnB command interpreter"""
+    """
+    AirBnB command interpreter
+    """
 
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit program"""
+        """
+        Quit command to exit program
+        """
         return (True)
 
     def do_EOF(self, arg):
-        """Exit prgram when EOF (Ctrl+D) is encountered"""
+        """
+        Exit prgram when EOF (Ctrl+D) is encountered
+        """
         print("")
         return (True)
 
     def emptyline(self):
-        """Do nothing when an empty line is entered"""
+        """
+        Do nothing when an empty line is entered
+        """
         pass
 
 
