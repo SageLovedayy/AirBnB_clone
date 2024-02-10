6b4ab14fc5dfe6bbe6fa6a198451431338b3f879
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-10 21:12:52 +0100

    Add Place, State, City, Amenity and Review classes

--- console.py
+++ console.py
@@ -144,7 +144,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """Exit the program when EOF (Ctrl+D) is encountered"""
-        print("")  # Print a newline for better formatting
+        print("")
         return True
 
     def help_EOF(self):
