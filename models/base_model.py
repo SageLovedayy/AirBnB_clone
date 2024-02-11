8f6bfe3c9fc90716bb87b9492c7addeed52216ba
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-11 18:24:11 +0100

    Debug 6:24

--- models/base_model.py
+++ models/base_model.py
@@ -66,6 +66,6 @@ class BaseModel():
                 new_dict[key] = value.isoformat()
             else:
                 new_dict[key] = value
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        # new_dict["created_at"] = self.created_at.isoformat()
+        # new_dict["updated_at"] = self.updated_at.isoformat()
         return (new_dict)
