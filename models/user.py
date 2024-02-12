e7c688e5531cffa496f711d1ec5de7a8cf8561fc
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-12 02:01:24 +0100

    debug 12/2-2:01

--- models/user.py
+++ models/user.py
@@ -7,16 +7,16 @@ class User(BaseModel):
     """
     user class, an instance of base model
     """
-    # def __init__(self, *args, **kwargs):
-    #    """
-    #    Initializes user instance with email. password
-    #    , first_name and last_name attributes
-    #    """
-    #    super().__init__(*args, **kwargs)
-    #    self.email = kwargs.get('email', "")
-    #    self.password = kwargs.get('password', "")
-    #    self.first_name = kwargs.get('first_name', "")
-    #    self.last_name = kwargs.get('last_name', "")
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes user instance with email. password
+        , first_name and last_name attributes
+        """
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email', "")
+        self.password = kwargs.get('password', "")
+        self.first_name = kwargs.get('first_name', "")
+        self.last_name = kwargs.get('last_name', "")
 
-    state_id = ""
-    name = ""
+    #state_id = ""
+    #name = ""
