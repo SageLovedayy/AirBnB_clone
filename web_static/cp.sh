ef4240d34c74420c1738cddda47f8984b4f7b219
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-02-19 23:48:33 +0100

    Add web static files

---
+++ web_static/cp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp styles/102-common.css styles/103-common.css
+cp styles/102-header.css styles/103-header.css
+cp styles/102-footer.css styles/103-footer.css
+cp styles/102-filters.css styles/103-filters.css
+cp styles/102-places.css styles/103-places.css
